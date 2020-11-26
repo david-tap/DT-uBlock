f3939e644923d1c7841c081842bc9c21234d865c
david-tap <dtapuchi@gmail.com>
2020-11-26 10:49:17 -0500

    Update atcol.js

--- atcol.js
+++ atcol.js
@@ -1,13 +1,17 @@
-(() => {
-    'use strict';
-    const fix = ev => {
+'use strict';
+/// alias at.js
+(function() {
+     const fix = ev => {
                     if (ev) { window.removeEventListener(ev.type, fix, true); }
                     try {
                         const framed = document.getElementById("PageContainerFrame");
                         const frame2 = framed.contentWindow.document.getElementById("frame2");
-                        frame2.setAttribute("cols","1px,*");
+                        frame2.setAttribute("cols","1px,*"); 
                     } catch { }
     };
-          fix();
-
+    if (document.readyState === 'loading') {
+              window.addEventListener('DOMContentLoaded', fix, true);
+    } else {
+     fix();
+    }
 })();
